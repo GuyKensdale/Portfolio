5eb59589849ed365aa0e7899a81a4c330430f82f
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-07-04 14:50:28 +0100

    scroll snap and lottie container fix nav bar bottom added

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import animationData from "./assests/computer.json";
 import "./App.css";
 import { useRef } from "react";
-import useScrollSnap from "react-use-scroll-snap";
+import { VscGithub } from "react-icons/vsc";
+import { AiFillLinkedin } from "react-icons/ai";
+
 const App = () => {
-  const scrollRef = useRef(null);
-  useScrollSnap({ ref: scrollRef, duration: 50, delay: 20 });
   const computerRef = useRef<LottieRefCurrentProps>(null);
   const handleLocalGitClick = () => {
     window.location.replace("https://github.com/GuyKensdale/Local-Link");
@@ -24,38 +24,48 @@ const App = () => {
   const handleChatGitClick = () => {
     window.location.replace("https://github.com/GuyKensdale/Chat-Spike-");
   };
+  const gitHubClick = () => {
+    window.location.replace("https://github.com/GuyKensdale");
+  };
+  const linkedInCLick = () => {
+    window.location.replace(
+      "https://www.linkedin.com/in/guy-kensdale-805829145/"
+    );
+  };
 
   return (
     <div className="container">
-      <h1>Guy Kensdale</h1>
-      <h2>Full Stack Developer</h2>
+      <section id="homePage">
+        <div className="homeHead">
+          <h1>Guy Kensdale</h1>
+          <h2>Full Stack Developer</h2>
 
-      <ul>
-        <li>
-          <a href="#experience">Technical Skills</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#about">About Me</a>
-        </li>
-      </ul>
-
-      <div className="lottie-container">
-        <div id="lottieAnimation">
-          <Lottie
-            onComplete={() => {
-              computerRef.current?.goToAndPlay(104, true);
-            }}
-            lottieRef={computerRef}
-            loop={false}
-            animationData={animationData}
-          />
+          <ul>
+            <li>
+              <a href="#experience">Technical Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#about">About Me</a>
+            </li>
+          </ul>
         </div>
-      </div>
-
-      <section ref={scrollRef} id="experience">
+        <div className="lottie-container">
+          <div id="lottieAnimation">
+            <Lottie
+              onComplete={() => {
+                computerRef.current?.goToAndPlay(104, true);
+              }}
+              lottieRef={computerRef}
+              loop={false}
+              animationData={animationData}
+            />
+          </div>
+        </div>
+      </section>
+      <section id="experience">
         <ul>
           <li>
             <a href="#experience">Technical Skills</a>
@@ -127,7 +137,7 @@ const App = () => {
         </div>
       </section>
 
-      <section ref={scrollRef} id="projects">
+      <section id="projects">
         <ul>
           <li>
             <a href="#experience">Technical Skills</a>
@@ -141,16 +151,18 @@ const App = () => {
         </ul>
         <h2>Projects</h2>
         <h3>LocaLink</h3>
-        <p>
-          LocaLink is a mobile app I have co-created to unite people living in
-          the same area or buildings without the hassle of sharing personal
-          information, such as email or phone number, with people you don’t know
-          or have just met, which is often the case on other social media
-          platforms. LocaLink aims to be the one-stop shop for online community
-          interaction, avoiding the need for several groups and pages across
-          many different platforms. Making all the information about a building
-          or neighbourhood organised and accessible
-        </p>
+        <div className="centrePTag">
+          <p>
+            LocaLink is a mobile app I have co-created to unite people living in
+            the same area or buildings without the hassle of sharing personal
+            information, such as email or phone number, with people you don’t
+            know or have just met, which is often the case on other social media
+            platforms. LocaLink aims to be the one-stop shop for online
+            community interaction, avoiding the need for several groups and
+            pages across many different platforms. Making all the information
+            about a building or neighbourhood organised and accessible
+          </p>
+        </div>
         <div className="centreButton">
           <button className="button-63" onClick={handleLocalGitClick}>
             GitHub Repo
@@ -160,12 +172,14 @@ const App = () => {
           </button>
         </div>
         <h3>Guys News</h3>
-        <p>
-          Guys News is a full stack project i created, allowing users to view
-          news articles from a home page, within this website you can leave
-          comments and up vote articles and these actions work directly with a
-          back end psql database.as well as navigate through various articles.
-        </p>
+        <div className="centrePTag">
+          <p>
+            Guys News is a full stack project i created, allowing users to view
+            news articles from a home page, within this website you can leave
+            comments and up vote articles and these actions work directly with a
+            back end psql database.as well as navigate through various articles.
+          </p>
+        </div>
         <div className="centreButton">
           <button className="button-63" onClick={handleFENewsGitClick}>
             FE GitHub Repo
@@ -175,13 +189,16 @@ const App = () => {
           </button>
         </div>
         <h3>Chat room</h3>
-        <p>
-          Chat room is as you might have guessed a chat room, this allows anyone
-          with the repo to chat with display names rendered from a google sign
-          in. the firebase back end is available on the GitHub meaning everyone
-          will share the same chat room however, this project is a great
-          building block for and social media styled website or application.
-        </p>
+        <div className="centrePTag">
+          <p>
+            Chat room is as you might have guessed a chat room, this allows
+            anyone with the repo to chat with display names rendered from a
+            google sign in. the firebase back end is available on the GitHub
+            meaning everyone will share the same chat room however, this project
+            is a great building block for and social media styled website or
+            application.
+          </p>
+        </div>
         <div className="centreButton">
           <button className="button-63" onClick={handleChatGitClick}>
             GitHub Repo
@@ -189,7 +206,7 @@ const App = () => {
         </div>
       </section>
 
-      <section ref={scrollRef} id="about">
+      <section id="about">
         <ul>
           <li>
             <a href="#experience">Technical Skills</a>
@@ -202,20 +219,34 @@ const App = () => {
           </li>
         </ul>
         <h2>About Me</h2>
-        <p>
-          Passionate about coding, I possess a profound curiosity for the
-          intricacies of binary and the fundamental principles of programming.
-          With expertise in TypeScript, React, and React Native, I specialize as
-          a versatile full-stack coder. My drive to overcome obstacles and
-          develop efficient code fuels my satisfaction in bringing innovative
-          solutions to life. Notably, I contributed to the successful
-          development of the "Local Link" app, implementing tailored features
-          for diverse user types. Collaborating with fellow developers has
-          enriched my perspective and deepened my understanding of coding. Eager
-          to embrace evolving challenges, I am dedicated to a long-term coding
-          career, continuously learning, growing, and transforming ideas into
-          reality
-        </p>
+        <div className="centrePTag">
+          <p>
+            Passionate about coding, I possess a profound curiosity for the
+            intricacies of binary and the fundamental principles of programming.
+            With expertise in TypeScript, React, and React Native, I specialize
+            as a versatile full-stack coder. My drive to overcome obstacles and
+            develop efficient code fuels my satisfaction in bringing innovative
+            solutions to life. Notably, I contributed to the successful
+            development of the "Local Link" app, implementing tailored features
+            for diverse user types. Collaborating with fellow developers has
+            enriched my perspective and deepened my understanding of coding.
+            Eager to embrace evolving challenges, I am dedicated to a long-term
+            coding career, continuously learning, growing, and transforming
+            ideas into reality
+          </p>
+        </div>
+        <ul>
+          <li>
+            <button onClick={gitHubClick}>
+              <VscGithub></VscGithub>
+            </button>
+          </li>
+          <li>
+            <button onClick={linkedInCLick}>
+              <AiFillLinkedin></AiFillLinkedin>
+            </button>
+          </li>
+        </ul>
       </section>
     </div>
   );
